2ce713b86decb019f2b368f61b440d05b153473f
Seb Stuck <sebas7236@icloud.com>
2023-03-15 12:43:56 -0600

    Add project files.

---
+++ 2_1_23 daily code/_2_1_23_daily_code.py
@@ -0,0 +1,23 @@
+import random as r
+num1 = 40
+num2 = 2
+                              
+
+#a while loop that prints out the numbers from 40 down to 20 by 2's
+while num1 >= 20:
+    print(num1, end = " ")
+    num1 -= 2
+print(" ")
+#a while loop that prints out the numbers from from 2 to 2048 by multiples of 2
+print(" ")
+while num2 <= 2048:
+    print(num2, end = " ")
+    num2 *= 2
+print(" ")
+#a while loop that prints "FROGS!" to the screen until the user types "turtle", at which point is says, "ok fine, have a turtle."
+print(" ")
+while FROGS != "turtles":
+    print(FROGS)
+    FROGS 
+    FROGS = input("Type in turtle if you really really really want a turtle.")
+print("Fine. Have a turtle.")
